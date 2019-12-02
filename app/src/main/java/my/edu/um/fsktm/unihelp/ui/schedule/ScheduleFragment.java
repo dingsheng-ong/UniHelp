0d92c45c807542ebd22c2f6638d6f24fc6f7242e
dyeoh <desmondyeoh11@gmail.com>
2019-12-02 10:48:21 +0800

    FUCK U CHI JUN

--- app/src/main/java/my/edu/um/fsktm/unihelp/ui/schedule/ScheduleFragment.java
+++ app/src/main/java/my/edu/um/fsktm/unihelp/ui/schedule/ScheduleFragment.java
@@ -4,20 +4,114 @@ package my.edu.um.fsktm.unihelp.ui.schedule;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
 import my.edu.um.fsktm.unihelp.R;
+import my.edu.um.fsktm.unihelp.models.ScheduleItem;
+import my.edu.um.fsktm.unihelp.ui.schedule.adapters.ScheduleAdapter;
 
 public class ScheduleFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.schedule_fragment, container, false);
+        View view = inflater.inflate(R.layout.schedule_fragment, container, false);
+
+        recyclerView = view.findViewById(R.id.schedule_item_list);
+
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        List<ScheduleItem> database = queryListOfItems();
+        adapter = new ScheduleAdapter(getContext(), database);
+        recyclerView.setAdapter(adapter);
+
+        return view;
+    }
+
+    private List<ScheduleItem> queryListOfItems() {
+        // TO-DO: query db
+        List<ScheduleItem> database = new ArrayList<>();
+        database.add(new ScheduleItem(
+                "divider",
+                "25th",
+                "",
+                new Timestamp(new GregorianCalendar(2019,12,1,16,0).getTimeInMillis()),
+                new Timestamp(new GregorianCalendar(2019,12,1,17,0).getTimeInMillis()),
+                "",
+                ""
+        ));
+        database.add(new ScheduleItem(
+                "class",
+                "WIX3004 Mobile Application Development - Lecture",
+                "",
+                new Timestamp(new GregorianCalendar(2019,12,1,16,0).getTimeInMillis()),
+                new Timestamp(new GregorianCalendar(2019,12,1,17,0).getTimeInMillis()),
+                "DK1, FSKTM",
+                "Dr. Ong Sim Ying"
+        ));
+        database.add(new ScheduleItem(
+                "class",
+                "WIX3004 Mobile Application Development - Lab",
+                "",
+                new Timestamp(new GregorianCalendar(2019,12,1,16,0).getTimeInMillis()),
+                new Timestamp(new GregorianCalendar(2019,12,1,17,0).getTimeInMillis()),
+                "DK1, FSKTM",
+                "Dr. Ong Sim Ying"
+        ));
+        database.add(new ScheduleItem(
+                "divider",
+                "26th",
+                "",
+                new Timestamp(new GregorianCalendar(2019,12,1,16,0).getTimeInMillis()),
+                new Timestamp(new GregorianCalendar(2019,12,1,17,0).getTimeInMillis()),
+                "",
+                ""
+        ));
+        database.add(new ScheduleItem(
+                "event",
+                "Industrial Training Talk 2019/2020",
+                "EVERY SINGLE STUDENT MUST JOIN or cannot graduate!!!!!!",
+                new Timestamp(new GregorianCalendar(2019,12,1,16,0).getTimeInMillis()),
+                new Timestamp(new GregorianCalendar(2019,12,1,17,0).getTimeInMillis()),
+                "The Cube, FSKTM",
+                null
+        ));
+        database.add(new ScheduleItem(
+                "booking",
+                "",
+                "",
+                new Timestamp(new GregorianCalendar(2019,12,1,16,0).getTimeInMillis()),
+                new Timestamp(new GregorianCalendar(2019,12,1,17,0).getTimeInMillis()),
+                "Student Center's Discussion Room",
+                null
+        ));
+        // ADD EMPTY SPACE AT END
+        database.add(new ScheduleItem(
+                "divider",
+                "",
+                "",
+                new Timestamp(new GregorianCalendar(2019,12,1,16,0).getTimeInMillis()),
+                new Timestamp(new GregorianCalendar(2019,12,1,17,0).getTimeInMillis()),
+                "",
+                ""
+        ));
+        return database;
     }
 
 }
---
+++ app/src/main/java/my/edu/um/fsktm/unihelp/ui/schedule/adapters/ScheduleAdapter.java
@@ -0,0 +1,114 @@
+package my.edu.um.fsktm.unihelp.ui.schedule.adapters;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+
+import my.edu.um.fsktm.unihelp.R;
+import my.edu.um.fsktm.unihelp.models.ScheduleItem;
+
+public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.ItemViewHolder> {
+
+    private List<ScheduleItem> database;
+    private Context context;
+
+    //  View Holder
+    static class ItemViewHolder extends RecyclerView.ViewHolder {
+        View view;
+        ItemViewHolder(View view) {
+            super(view);
+            this.view = view;
+        }
+    }
+
+    public ScheduleAdapter(Context context, List<ScheduleItem> database) {
+        this.context = context;
+        this.database = database;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.schedule_item,
+                parent,
+                false
+        );
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ItemViewHolder holder, int position) {
+        ScheduleItem item = database.get(position);
+        View root = holder.view;
+        LinearLayout itemLayout = root.findViewById(R.id.schedule_item);
+        TextView title = root.findViewById(R.id.schedule_item_title);
+        TextView description = root.findViewById(R.id.schedule_item_description);
+
+        // Formatting
+        String descriptionText;
+        switch(item.getType()) {
+            case "class":
+                itemLayout.setBackgroundColor(Color.parseColor("#3f51b5"));
+                itemLayout.setPadding(30,30,30,30);
+                title.setTextColor(Color.WHITE);
+                description.setTextColor(Color.WHITE);
+                descriptionText =
+                        new SimpleDateFormat("h:mm a").format(item.getTimeStart()) + " - " +
+                                new SimpleDateFormat("h:mm a").format(item.getTimeEnd()) + "\n" +
+                                item.getVenue() + "\n" +
+                                item.getLecturer();
+                title.setText(item.getTitle());
+                description.setText(descriptionText);
+                break;
+            case "event":
+                itemLayout.setBackgroundColor(Color.parseColor("#00cc66"));
+                itemLayout.setPadding(30,30,30,30);
+                title.setTextColor(Color.WHITE);
+                description.setTextColor(Color.WHITE);
+                descriptionText =
+                        new SimpleDateFormat("h:mm a").format(item.getTimeStart()) + " - " +
+                                new SimpleDateFormat("h:mm a").format(item.getTimeEnd()) + "\n" +
+                                item.getVenue() + "\n" +
+                                item.getDescription();
+                title.setText(item.getTitle());
+                description.setText(descriptionText);
+                break;
+            case "booking":
+                itemLayout.setBackgroundColor(Color.parseColor("#008fb3"));
+                itemLayout.setPadding(30,30,30,30);
+                title.setTextColor(Color.WHITE);
+                description.setTextColor(Color.WHITE);
+                descriptionText =
+                        new SimpleDateFormat("h:mm a").format(item.getTimeStart()) + " - " +
+                                new SimpleDateFormat("h:mm a").format(item.getTimeEnd());
+                title.setText(item.getVenue());
+                description.setText(descriptionText);
+                break;
+            case "divider":
+                title.setTextColor(Color.GRAY);
+                title.setTextSize(25);
+                title.setText(item.getTitle());
+                description.setVisibility(TextView.GONE);
+                break;
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return database.size();
+    }
+}
